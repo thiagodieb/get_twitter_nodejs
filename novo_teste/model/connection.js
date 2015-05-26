33f435fa90368a590af70e42196c2f7875436006
Thiago Dieb <thiago@dieb.com.br>
2015-05-25 23:22:32 -0300

    alteracao de path

--- novo_teste/model/connection.js
+++
@@ -1,7 +0,0 @@
-var connection = require('mongoose');
-connection.connect('mongodb://localhost:27017/teste');
-var db = connection.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-
-
-module.exports = connection;
