cc479cf195f7f34e40411cd237b1442648c771e3
root <root@SERVER37.server1.com>
2015-12-25 14:41:56 -0600

    correcoa no endereco do mongodb

--- admin/models/connection.js
+++ admin/models/connection.js
@@ -1,5 +1,5 @@
 var connection = require('mongoose');
-connection.connect('mongodb://localhost:27017/teste');
+connection.connect('mongodb://localhost:27017/collector');
 connection.connection.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = connection;
